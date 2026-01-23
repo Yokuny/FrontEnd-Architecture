74feaed8bdeebf342bb30e7b08d48d42b7208940
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2026-01-23 17:14:28 -0300

    feat: contract list redirect

--- src/routes/_private/contracts/contract-list/index.tsx
+++ src/routes/_private/contracts/contract-list/index.tsx
@@ -1,18 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { useTranslation } from 'react-i18next';
-import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_private/contracts/contract-list/')({
-  component: ContractListPage,
+  beforeLoad: () => {
+    throw redirect({
+      to: '/register/contracts',
+      search: { page: 1, size: 10, search: undefined },
+    });
+  },
 });
-
-function ContractListPage() {
-  const { t } = useTranslation();
-
-  return (
-    <Card>
-      <CardHeader title={t('contract.list')} />
-      <CardContent>{/* TODO: Migrate content from iotlog-fronted/src/pages/register/contract/view/ListContract.jsx */}</CardContent>
-    </Card>
-  );
-}
