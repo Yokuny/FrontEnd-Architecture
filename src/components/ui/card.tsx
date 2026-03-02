a2ccd81745c1013efa9cba680b6e8c028e0ad7ea
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2026-03-01 22:40:24 -0300

    fix: style

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,8 +1,9 @@
 import type { ComponentProps } from 'react';
-import { cn } from '@/lib/utils';
+
+import { cn } from '@/lib/utils/index';
 
 function Card({ className, ...props }: ComponentProps<'div'>) {
-  return <div data-slot="card" className={cn('flex w-full flex-col gap-3', className)} {...props} />;
+  return <div data-slot="card" className={cn('flex flex-col gap-6 rounded-xl border bg-card py-6 text-card-foreground', className)} {...props} />;
 }
 
 function CardHeader({ className, ...props }: ComponentProps<'div'>) {
@@ -10,7 +11,7 @@ function CardHeader({ className, ...props }: ComponentProps<'div'>) {
     <div
       data-slot="card-header"
       className={cn(
-        '@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 rounded-md border bg-card px-2 py-2 has-data-[slot=card-action]:grid-cols-[1fr_auto] md:px-6 md:py-3 [.border-b]:pb-6',
+        '@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-2 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6',
         className,
       )}
       {...props}
@@ -19,11 +20,11 @@ function CardHeader({ className, ...props }: ComponentProps<'div'>) {
 }
 
 function CardTitle({ className, ...props }: ComponentProps<'div'>) {
-  return <div data-slot="card-title" className={cn('ml-2 font-semibold leading-none md:ml-0', className)} {...props} />;
+  return <div data-slot="card-title" className={cn('font-semibold leading-none', className)} {...props} />;
 }
 
 function CardDescription({ className, ...props }: ComponentProps<'div'>) {
-  return <div data-slot="card-description" className={cn('hidden bg-card font-mono text-muted-foreground text-sm md:block', className)} {...props} />;
+  return <div data-slot="card-description" className={cn('text-muted-foreground text-sm', className)} {...props} />;
 }
 
 function CardAction({ className, ...props }: ComponentProps<'div'>) {
@@ -31,11 +32,11 @@ function CardAction({ className, ...props }: ComponentProps<'div'>) {
 }
 
 function CardContent({ className, ...props }: ComponentProps<'div'>) {
-  return <div data-slot="card-content" className={cn('rounded-md border bg-card py-4', className)} {...props} />;
+  return <div data-slot="card-content" className={cn('px-6', className)} {...props} />;
 }
 
 function CardFooter({ className, ...props }: ComponentProps<'div'>) {
-  return <div data-slot="card-footer" className={cn('flex items-center rounded-md border bg-card py-4 md:px-6 [.border-t]:pt-6', className)} {...props} />;
+  return <div data-slot="card-footer" className={cn('flex items-center px-6 [.border-t]:pt-6', className)} {...props} />;
 }
 
-export { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle };
+export { Card, CardHeader, CardFooter, CardTitle, CardAction, CardDescription, CardContent };
